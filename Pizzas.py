97d68ad2f8e41306c4f8e3d73839c36adb4a63db
Asrivast1 <askandshankar2001@gmail.com>
2021-02-23 18:00:49 +0530

    Almost done

--- Pizzas.py
+++ Pizzas.py
@@ -21,31 +21,44 @@ for i in lst_1:
         if i == j:
             x = lst_1.index(i)
             lst_1[x].clear()
+print(lst_1)
 lst_two = []
 lst_three = []
 lst_four = []
-for i in range(len(lst_1)):
-    if len(lst_1[i:])>=2 & (lst_1[i] & lst_1[i+1] != []) & (sorted(lst_1[i]) != sorted(lst_1[i+1])):
-        while(len(lst_two)< 2*team_two):
-            lst_two.append(lst_1[i])
-            lst_two.append(lst_1[i+1])
+for i in range(len(lst_1)): lst_1[i].insert(0, i+1)
+print(lst_1)
+print(len(lst_1))
+for a in range(0, len(lst_1), 3) :
+    try:
+        while len(lst_1[a])>1:
+            lst_two.append(lst_1[a])
             break
-        break
-    if len(lst_1[i:])>=3 & (lst_1[i] & lst_1[i+1] & lst_1[i+2] != []) & (sorted(lst_1[i]) != sorted(lst_1[i+1]) != sorted(lst_1[i+2])):
-        while(len(lst_three)< 3*team_three):
-            lst_three.append(lst_1[i])
-            lst_three.append(lst_1[i+1])
-            lst_three.append(lst_1[i+2])
+        while len(lst_1[a+1])>1:
+            lst_three.append(lst_1[a+1])
             break
-        break
-    if len(lst_1[i:]) >= 4 & (lst_1[i] & lst_1[i+1] & lst_1[i+2] & lst_1[i+3] != []) & (sorted(lst_1[i]) != sorted(lst_1[i+1]) != sorted(lst_1[i+2]) != sorted(lst_1[i+3])):
-        while(len(lst_four)< 4*team_four):
-            lst_four.append(lst_1[i])
-            lst_four.append(lst_1[i+1])
-            lst_four.append(lst_1[i+2])
-            lst_four.append(lst_1[i+3])
+        while len(lst_1[a+2])>1:
+            lst_four.append(lst_1[a+2])
             break
-        break
-    continue
+    except:pass
 print(lst_two)
 print(lst_three)
+print(lst_four)
+a = len(lst_two)%2
+b = len(lst_three)%3
+c = len(lst_four)%4
+if a or b or c !=0:
+    lst_two.pop()
+    for i in range(b): lst_three.pop()
+    for i in range(c): lst_four.pop()
+else:pass
+print(lst_two)
+print(lst_three)
+print(lst_four)
+delivery = len(lst_two)+len(lst_three)+len(lst_four)
+print(delivery)
+for i in range(0, len(lst_two), 2):
+    print(2, lst_two[i][0], lst_two[i+1][0])
+for i in range(0, len(lst_three), 3):
+    print(3, lst_three[i][0], lst_three[i+1][0], lst_three[i+2][0])
+for i in range(0, len(lst_four), 4):
+    print(4, lst_four[i][0], lst_four[i+1][0], lst_four[i+2][0], lst_four[i+3][0])
